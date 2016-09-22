8d10e012b700c505163e85f38ebff70560507db7
bin.shen <bluelazysb@hotmail.com>
2016-09-22 14:40:33 +0800

    123123

--- airtree_swift/ViewController.swift
+++ airtree_swift/ViewController.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIAlertViewDelegate {
 
     @IBOutlet weak var TxtUsername: UITextField!
     @IBOutlet weak var TxtPassword: UITextField!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -29,19 +29,24 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
     @IBAction func clickLoginButton(_ sender: UIButton) {
-
-        let username = TxtUsername.text;
-        let password = TxtPassword.text;
-
-        print(username)
-        print(password)
-
+        let username = TxtUsername.text!;
+        let password = TxtPassword.text!;
         let baseURL = "http://api.7drlb.com";
         let params = ["username": username, "password": password]
         let request = MKNetworkRequest(urlString: baseURL + "/user/login", params: params, bodyData: nil, httpMethod: "POST");
         request? .addCompletionHandler { response in
-            print(response?.responseAsString)
-
+            let jsonStr = response?.responseAsString
+            var data = jsonStr!.data(using: .utf8)!
+            if let parsedData = try? JSONSerialization.jsonObject(with: data) as! [String:Any] {
+                let user = parsedData["user"] as? [String:Any]
+                if(user == nil) {
+                    let alert: UIAlertView = UIAlertView(title: "登录失败", message: "输入的用户名或密码错误.", delegate: self, cancelButtonTitle: "OK")
+                    alert.show()
+                } else {
+                    let nav = self.storyboard!.instantiateViewController(withIdentifier: "NavMainViewController")
+                    self.present(nav, animated: true)
+                }
+            }
         }
         let engine = MKNetworkHost()
         engine.start(request)
