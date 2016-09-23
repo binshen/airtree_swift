1ceabf2c8c94c672feab1d8d7e1c1746911bb2b8
bin.shen <bluelazysb@hotmail.com>
2016-09-23 17:19:38 +0800

    123123

--- airtree_swift/DeviceDetailReviseController.swift
+++ airtree_swift/DeviceDetailReviseController.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class DeviceDetailReviseController: UIViewController {
+    
+    @IBOutlet weak var TextDeviceName: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let device = _selectedDevice as? NSDictionary
+        if device?.value(forKey: "name") != nil {
+            self.TextDeviceName.text = device?.value(forKey: "name") as! String
+        } else {
+            self.TextDeviceName.text = device?.value(forKey: "mac") as! String
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +28,35 @@ class DeviceDetailReviseController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func clickUpdate(_ sender: UIButton) {
+        if !self.TextDeviceName.hasText {
+            let alert: UIAlertView = UIAlertView(title: "错误信息", message: "请输入设备名称.", delegate: self, cancelButtonTitle: "OK")
+            alert.show()
+        } else {
+            let userID = _loginUser["_id"] as! String
+            let device = _selectedDevice as? NSDictionary
+            let deviceID = device!.value(forKey: "_id") as! String
+            let deviceName = self.TextDeviceName.text!
+            let params = ["name": deviceName]
+            let request = MKNetworkRequest(urlString: MORAL_API_BASE_PATH + "/user/\(userID)/device/\(deviceID)/update_name", params: params, bodyData: nil, httpMethod: "POST");
+            request? .addCompletionHandler { response in
+                let jsonStr = response?.responseAsString
+                let data = jsonStr!.data(using: .utf8)!
+                if let parsedData = try? JSONSerialization.jsonObject(with: data) as! [String:Any] {
+                    let success = parsedData["success"] as? Bool
+                    if success! {
+                        //_selectedDevice.set(deviceName, forKey: "name")
+                        self.navigationController?.popViewController(animated: true)
+                    } else {
+                        let alert: UIAlertView = UIAlertView(title: "错误信息", message: parsedData["error"] as? String, delegate: self, cancelButtonTitle: "OK")
+                        alert.show()
+                    }
+                }
+            }
+            let engine = MKNetworkHost()
+            engine.start(request)
+        }
+    }
 
     /*
     // MARK: - Navigation
