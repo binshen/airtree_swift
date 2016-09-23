6c42c431a3d156aa904a6753bfa2a7214f04aaea
bin.shen <bluelazysb@hotmail.com>
2016-09-23 10:56:00 +0800

    123123

--- airtree_swift/DeviceInfoController.swift
+++ airtree_swift/DeviceInfoController.swift
@@ -13,19 +13,31 @@ class DeviceInfoController: UITableViewController, UIAlertViewDelegate {
     @IBOutlet weak var bottomView: UIView!
     
     var timer: Timer!
+    var checkStatus: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "返回", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
         UINavigationBar.appearance().tintColor = UIColor.white
+
+        let device = _selectedDevice as? NSDictionary
+        let type = device?.value(forKey: "type") as? Int
+        if type == 1 {
+            self.autoRefreshData()
+        }
+
+        if false {
+            var frame = self.bottomView.frame
+            frame.size.height = 60
+            self.bottomView.frame = frame
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true);
 
-        var device = _selectedDevice as? NSDictionary
-
+        let device = _selectedDevice as? NSDictionary
         let indexPath = IndexPath(row: 1, section: 0)
         let cell = self.tableView.cellForRow(at: indexPath)
         cell?.detailTextLabel?.text = device?.value(forKey: "name") as? String
@@ -45,7 +57,36 @@ class DeviceInfoController: UITableViewController, UIAlertViewDelegate {
     }
 
     func autoRefreshData() {
+        let device = _selectedDevice as? NSDictionary
+        let mac = device?.value(forKey: "mac") as! String
+        let request = MKNetworkRequest(urlString: MORAL_API_BASE_PATH + "/device/mac/\(mac)/get_test", params: nil, bodyData: nil, httpMethod: "GET");
+        request? .addCompletionHandler { response in
+            let jsonStr = response?.responseAsString
+            let data = jsonStr!.data(using: .utf8)!
+            if let parsedData = try? JSONSerialization.jsonObject(with: data) as! [String:Any] {
+                let test = parsedData["test"] as! Int
+                let created = parsedData["created"] as! Double
+                if test >= 0 {
+                    var dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+                    let dateString = dateFormatter.string(from: Date(timeIntervalSince1970: created))
+                    self.checkStatus = test == 1 ? "需要更换(\(dateString))" : "无需更换(\(dateString))"
+                } else {
+                    self.checkStatus = ""
+                }
+            }
 
+            let indexPath = IndexPath(row: 5, section: 0)
+            let cell = self.tableView.cellForRow(at: indexPath)
+            cell?.detailTextLabel?.text = self.checkStatus
+            if false {
+                cell?.detailTextLabel?.font = UIFont.systemFont(ofSize: 15)
+            } else if false {
+                cell?.detailTextLabel?.font = UIFont.systemFont(ofSize: 14)
+            }
+        }
+        let engine = MKNetworkHost()
+        engine.start(request)
     }
 
     // MARK: - Table view data source
@@ -56,20 +97,34 @@ class DeviceInfoController: UITableViewController, UIAlertViewDelegate {
 //    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 6
+        let device = _selectedDevice as? NSDictionary
+        let type = device?.value(forKey: "type") as? Int
+        if type == 1 {
+            return 7
+        } else {
+            return 6
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        var device = _selectedDevice as? NSDictionary
+        let device = _selectedDevice as? NSDictionary
+        let type = device?.value(forKey: "type") as? Int
         let cell = tableView.dequeueReusableCell(withIdentifier: "DeviceInfoCell", for: indexPath)
+        cell.isUserInteractionEnabled = false
+        cell.accessoryType = UITableViewCellAccessoryType.none
         switch indexPath.row {
             case 0:
                 cell.textLabel?.text = "设备编码"
                 cell.detailTextLabel?.text = device?.value(forKey: "_id") as? String
+                if false { //TODO
+                    cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 15)
+                }
             case 1:
                 cell.textLabel?.text = "设备名称"
+                cell.detailTextLabel?.text = device?.value(forKey: "name") as? String
+                cell.isUserInteractionEnabled = true
+                cell.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
             case 2:
                 cell.textLabel?.text = "类型"
                 cell.detailTextLabel?.text = device?.value(forKey: "type") as? String
@@ -79,11 +134,18 @@ class DeviceInfoController: UITableViewController, UIAlertViewDelegate {
             case 4:
                 cell.textLabel?.text = "历史数据"
                 cell.detailTextLabel?.text = ""
+                cell.isUserInteractionEnabled = true
+                cell.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
             case 5:
-                cell.textLabel?.text = "滤网检测"
-                cell.detailTextLabel?.text = ""
+                if type == 1 {
+                    cell.textLabel?.text = "滤网检测"
+                } else {
+                    cell.textLabel?.text = ""
+                    cell.detailTextLabel?.text = ""
+                }
             case 6:
                 cell.textLabel?.text = ""
+                cell.detailTextLabel?.text = ""
             default:
                 break
         }
@@ -102,6 +164,17 @@ class DeviceInfoController: UITableViewController, UIAlertViewDelegate {
     }
 
     override public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        var device = _selectedDevice as? NSDictionary
+        let type = device?.value(forKey: "type") as? Int
+        if type == 1 {
+            if indexPath.row == 6 {
+                return 0
+            }
+        } else {
+            if indexPath.row == 5 {
+                return 0
+            }
+        }
         return 60
     }
 
