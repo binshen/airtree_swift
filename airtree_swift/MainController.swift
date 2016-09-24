dc4d286c1bb2f7a890b4dcc76a042d2b8e563be5
bin.shen <bluelazysb@hotmail.com>
2016-09-24 22:24:09 +0800

    123123

--- airtree_swift/MainController.swift
+++ airtree_swift/MainController.swift
@@ -17,9 +17,14 @@ class MainController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     
-    var contentList = [Any]()
-    var viewControllers = [Any]()
+    @IBOutlet weak var bottomView: UIView!
     
+    var contentList = [AnyObject]()
+    var viewControllers = [UIViewController?]()
+
+    var timer: Timer!
+    var numberPages: Int!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,17 +36,39 @@ class MainController: UIViewController, UIScrollViewDelegate {
 
         self.navigationItem.rightBarButtonItem = nil;
 
+        self.scrollView.isPagingEnabled = true
+        self.scrollView.showsHorizontalScrollIndicator = false
+        self.scrollView.showsVerticalScrollIndicator = false
+        //self.scrollView.bounds = true
+        self.scrollView.scrollsToTop = false
+        //self.scrollView.autoresizingMask = true
+        self.scrollView.delegate = self
 
-        print("-----------------------------------123")
+        var doubleTap = UITapGestureRecognizer(target: self, action: #selector(doDoubleTap))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.numberOfTouchesRequired = 1
+        self.scrollView.addGestureRecognizer(doubleTap)
+
+        self.pageControl.hidesForSinglePage = true
+        self.pageControl.isUserInteractionEnabled = false
+        self.pageControl.currentPage = 0
+        self.pageControl.transform = CGAffineTransform(scaleX:1.2, y:1.2)
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated);
+        super.viewWillAppear(true);
 
         self.initHomePage()
+        self.timer = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(autoRefreshData), userInfo: nil, repeats: true)
 
-
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = true
     }
+
+    override func viewDidAppear(_ animated: Bool) {
+        //self.scrollView.contentSize = CGSize(width: self.scrollView.frame.size.width * CGFloat(self.numberPages), height: self.scrollView.frame.size.height)
+        super.viewDidAppear(animated)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -49,7 +76,71 @@ class MainController: UIViewController, UIScrollViewDelegate {
     }
     
     func initHomePage() {
-        print("+++++++++++++++++++++++++++++++++++234")
+        let userID = _loginUser["_id"] as! String
+        let url = MORAL_API_BASE_PATH + "/user/\(userID)/get_device"
+        let request = MKNetworkRequest(urlString: url, params: nil, bodyData: nil, httpMethod: "GET");
+        request? .addCompletionHandler { response in
+            let jsonStr = response?.responseAsString
+            let data = jsonStr!.data(using: .utf8)!
+            if let parsedData = try? JSONSerialization.jsonObject(with: data) as![AnyObject] {
+                self.contentList = parsedData
+
+                //self.scrollView.subviews
+                self.numberPages = self.contentList.count
+
+                var controllers = [UIViewController?]()
+                for i in 0...self.numberPages {
+                    controllers.append(nil)
+                }
+                self.viewControllers = controllers
+
+                self.scrollView.contentSize = CGSize(width: self.scrollView.frame.size.width * CGFloat(self.numberPages), height: self.scrollView.frame.size.height)
+
+                self.pageControl.numberOfPages = self.numberPages
+                self.pageControl.isHidden = false
+                
+                if self.pageControl.currentPage < 1 {
+                    let device = self.contentList[0] as? NSDictionary
+                    self.navigationItem.title = device?.value(forKey: "name") as? String == nil ? device?.value(forKey: "mac") as? String : device?.value(forKey: "name") as! String
+                }
+
+                for i in 0...self.numberPages {
+                    self.loadScrollViewWithPage(i)
+                }
+            }
+
+            self.spinner.stopAnimating()
+        }
+        self.spinner.transform = CGAffineTransform(scaleX:1.5, y:1.5)
+        self.spinner.center = self.view.center
+        self.spinner.startAnimating()
+
+        let engine = MKNetworkHost()
+        engine.start(request)
+    }
+
+    func loadScrollViewWithPage(_ page: Int) {
+        if page >= self.contentList.count {
+            return
+        }
+
+        var controller = self.viewControllers[page]
+        if controller == nil {
+            controller = self.storyboard?.instantiateViewController(withIdentifier: "DeviceViewController")
+            self.viewControllers[page] = controller
+        }
+
+        if controller?.view.superview == nil {
+
+        }
+    }
+
+    func autoRefreshData() {
+        self.initHomePage()
+    }
+
+    func doDoubleTap() {
+        print("*********************")
     }
 
     /*
